cb94af1a336a6f9c01f64b8178f16038cfc6f10f
JakeTurnick <jaketurnick@gmail.com>
2023-02-20 15:25:59 -0500

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True)
+    display_name: models.CharField(max_length=255)
+    avatar: models.ImageField(upload_to='profiles/')
